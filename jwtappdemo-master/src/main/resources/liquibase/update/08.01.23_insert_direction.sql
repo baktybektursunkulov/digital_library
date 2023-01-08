d724c857033827d38c69b9422017ff7f33c979bb
b.tursunkulov <@Zhakypali.Ayaulym>
2023-01-08 18:18:18 +0600

    insert table "direction"

---
+++ jwtappdemo-master/src/main/resources/liquibase/update/08.01.23_insert_direction.sql
@@ -0,0 +1,6 @@
+insert into direction(name, is_deleted) values ('Литература',false);
+insert into direction(name, is_deleted) values ('Информатика',false);
+insert into direction(name, is_deleted) values ('Геология',false);
+insert into direction(name, is_deleted) values ('Химия',false);
+insert into direction(name, is_deleted) values ('Биология',false);
+insert into direction(name, is_deleted) values ('Строительство',false);
