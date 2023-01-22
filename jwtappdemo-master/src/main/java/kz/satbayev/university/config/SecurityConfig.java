df4796ad7ec2f9bafa15de692446c980c9226f72
b.tursunkulov <@Zhakypali.Ayaulym>
2023-01-22 16:06:54 +0600

    add minio upload and download controller

--- jwtappdemo-master/src/main/java/kz/satbayev/university/config/SecurityConfig.java
+++ jwtappdemo-master/src/main/java/kz/satbayev/university/config/SecurityConfig.java
@@ -43,7 +43,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             "/webjars/**",
             // -- Swagger UI v3 (OpenAPI)
             "/v3/api-docs/**",
-            "/swagger-ui/**"
+            "/swagger-ui/**",
+            "/file/**"
             // other public endpoints of your API may be appended to this array
     };
     @Override
